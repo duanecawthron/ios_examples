2b07958fba27f3c05025fe96d55456bb84c6fa82
Duane Cawthron <duane@studiodc.com>
2011-12-27 15:57:10 -0600

    002_Calculator: add operation and enter buttons

--- 002_Calculator/Calculator/Calculator/CalculatorViewController.m
+++ 002_Calculator/Calculator/Calculator/CalculatorViewController.m
@@ -8,13 +8,37 @@
 
 #import "CalculatorViewController.h"
 
+@interface CalculatorViewController()
+@property (nonatomic) BOOL userIsInTheMiddleOfEnteringANumber;
+@end
+
 @implementation CalculatorViewController
 
 @synthesize display = _display;
+@synthesize userIsInTheMiddleOfEnteringANumber = _userIsInTheMiddleOfEnteringANumber;
 
 - (IBAction)digitPressed:(UIButton *)sender {
     NSString *digit = [sender currentTitle];
-    NSLog(@"digit pressed = %@", digit);
+    
+    // NSLog(@"digit pressed = %@", digit);
+
+    if (self.userIsInTheMiddleOfEnteringANumber) {
+        // UILabel *myDisplay = self.display; // or [self display];
+        // NSString *currentText = myDisplay.text; // or [myDisplay text];
+        // NSString *newText = [currentText stringByAppendingString:digit];
+        // myDisplay.text = newText; // or [myDisplay setText:newText];
+        // or
+        self.display.text = [self.display.text stringByAppendingString:digit];
+    } else {
+        self.display.text = digit;
+        self.userIsInTheMiddleOfEnteringANumber = YES;
+    }
+}
+
+- (IBAction)operationPressed:(UIButton *)sender {
+}
+
+- (IBAction)enterPressed {
 }
 
 @end
