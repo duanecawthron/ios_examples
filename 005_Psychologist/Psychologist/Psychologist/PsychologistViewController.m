14d22bd7854deb6af20bb49a989bc253be277333
Duane Cawthron <duane@studiodc.com>
2012-01-02 09:23:47 -0600

    005_Psychologist: add Dr. Pill

--- 005_Psychologist/Psychologist/Psychologist/PsychologistViewController.m
+++ 005_Psychologist/Psychologist/Psychologist/PsychologistViewController.m
@@ -27,6 +27,12 @@
 {
     if ([segue.identifier isEqualToString:@"ShowDiagnosis"]) {
         [segue.destinationViewController setHappiness:self.diagnosis];
+    } else if ([segue.identifier isEqualToString:@"Celebrity"]) {
+        [segue.destinationViewController setHappiness:100];
+    } else if ([segue.identifier isEqualToString:@"Serious"]) {
+        [segue.destinationViewController setHappiness:20];
+    } else if ([segue.identifier isEqualToString:@"TV Kook"]) {
+        [segue.destinationViewController setHappiness:50];
     }
 }
 
