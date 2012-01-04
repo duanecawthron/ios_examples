0a295ca2b9b5f88e5496cf4e0e75d5929120ad74
Duane Cawthron <duane@studiodc.com>
2012-01-04 10:51:37 -0600

    007_Imaginarium: add scalling

--- 007_Imaginarium/Imaginarium/Imaginarium/ImaginariumViewController.m
+++ 007_Imaginarium/Imaginarium/Imaginarium/ImaginariumViewController.m
@@ -8,7 +8,7 @@
 
 #import "ImaginariumViewController.h"
 
-@interface ImaginariumViewController()
+@interface ImaginariumViewController() <UIScrollViewDelegate>
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @end
@@ -17,11 +17,17 @@
 @synthesize scrollView;
 @synthesize imageView;
 
-- (void) viewDidLoad
+- (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.scrollView.delegate = self;
     self.scrollView.contentSize = self.imageView.image.size;
     self.imageView.frame = CGRectMake(0, 0, self.imageView.image.size.width, self.imageView.image.size.height);
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return self.imageView;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
