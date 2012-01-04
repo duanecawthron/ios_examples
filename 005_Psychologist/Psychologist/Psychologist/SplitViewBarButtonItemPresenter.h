3d5d8f616ce24203d0bc156d75637510ba5c431b
Duane Cawthron <duane@studiodc.com>
2012-01-04 10:02:31 -0600

    005_Psychologist: add RotatableViewController

---
+++ 005_Psychologist/Psychologist/Psychologist/SplitViewBarButtonItemPresenter.h
@@ -0,0 +1,13 @@
+//
+//  SplitViewBarButtonItemPresenter.h
+//  Psychologist
+//
+//  Created by cawthron on 1/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol SplitViewBarButtonItemPresenter <NSObject>
+@property (nonatomic, strong) UIBarButtonItem *splitViewBarButtonItem;
+@end
