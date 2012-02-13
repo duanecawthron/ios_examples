ead7801e4420ec86b474d3b007d84d285ce7eb5c
Duane Cawthron <duane@studiodc.com>
2012-02-13 17:25:47 -0600

    017_Flickr_Spots: new empty project

---
+++ 017_Flickr_Spots/Flickr/Flickr/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Flickr
+//
+//  Created by Duane Cawthron on 2/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
