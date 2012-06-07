28a5ec0d69b2b94b5b782a49cf3328e96e2b537e
Duane Cawthron <duane@studiodc.com>
2012-06-07 11:15:27 -0500

    033_addChildViewController: create subview with alloc/init, call addChildViewController

--- 033_addChildViewController/addChildViewController/addChildViewController/ViewController.m
+++ 033_addChildViewController/addChildViewController/addChildViewController/ViewController.m
@@ -27,12 +27,14 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 #if 0
+    // Case 1 - create SubViewController with alloc/init, do not call addChildViewController
     SubViewController *subViewController = [[SubViewController alloc] init];
     // alloc/init does not enable calling of any lifecycle methods
     
     View *view = [[View alloc] initWithFrame:self.view.bounds];
     view.backgroundColor = [UIColor yellowColor];
     subViewController.view = view;
+    subViewController.view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     [self.view addSubview:subViewController.view];
     
     // these are NOT called
@@ -46,11 +48,51 @@
     // ViewController touchesBegan
 #endif
 #if 0
+    // Case 2 - create SubViewController with alloc/init, call addChildViewController
+    SubViewController *subViewController = [[SubViewController alloc] init];
+    // alloc/init does not enable calling of any lifecycle methods
+    // - (void)viewDidLoad is not called
+    
+    View *view = [[View alloc] initWithFrame:self.view.bounds];
+    view.backgroundColor = [UIColor yellowColor];
+    subViewController.view = view;
+    subViewController.view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [self.view addSubview:subViewController.view];
+    
+    [self addChildViewController:subViewController];
+    // - (void)willMoveToParentViewController:(UIViewController *)parent
+    
+    // from this point on, subViewController works the same as case 4 below 
+    // NOTE addChildViewController: adds the SubViewController to the responder chain used to handle events
+    
+    [subViewController didMoveToParentViewController:self];
+    // - (void)didMoveToParentViewController:(UIViewController *)parent
+    
+    // these are called later in the run loop 
+    // - (void)viewWillAppear:(BOOL)animated
+    // - (void)viewDidLayoutSubviews
+    // - (void)viewDidAppear:(BOOL)animated
+    
+    // these are called when the device is rotated
+    // - (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+    // - (void)viewDidLayoutSubviews
+    // - (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
+    
+    // these are called when the subview is touched (not the buttons)
+    // View touchesBegan
+    // SubViewController touchesBegan, this is called because SubViewController was added to the responder chain
+    // View touchesBegan
+    // ViewController touchesBegan
+#endif
+#if 0
+    // Case 3 - create SubViewController from storyboard, do not call addChildViewController
     SubViewController *subViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"SubViewController"];
     // instantiating from the storyboard enables calling some lifecycle methods
     
     subViewController.view.frame = self.view.bounds;
     // - (void)viewDidLoad
+    
+    subViewController.view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     
     [self.view addSubview:subViewController.view];
     
@@ -70,6 +112,7 @@
     // ViewController touchesBegan
 #endif
 #if 1
+    // Case 4 - create SubViewController from storyboard, call addChildViewController
     SubViewController *subViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"SubViewController"];
     subViewController.view.frame = self.view.bounds;
     // - (void)viewDidLoad
@@ -93,7 +136,9 @@
     
     // these are called when the device is rotated
     // - (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+    // - (void)viewDidLayoutSubviews
     // - (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
+    
     // these are called when the subview is touched (not the buttons)
     // View touchesBegan
     // SubViewController touchesBegan, this is called because SubViewController was added to the responder chain
