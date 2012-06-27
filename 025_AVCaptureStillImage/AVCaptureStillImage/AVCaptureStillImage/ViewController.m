f08f9999476040be7147035d3380d4f91ba4aad7
Duane Cawthron <duane@studiodc.com>
2012-06-27 09:42:45 -0500

    allocate only one ALAssetsLibrary to stop memory leaks

--- 025_AVCaptureStillImage/AVCaptureStillImage/AVCaptureStillImage/ViewController.m
+++ 025_AVCaptureStillImage/AVCaptureStillImage/AVCaptureStillImage/ViewController.m
@@ -13,12 +13,23 @@
 @interface ViewController () <DCCaptureManagerDelegate>
 
 @property (nonatomic, strong) DCCaptureManager *captureManager;
+@property (nonatomic, strong) ALAssetsLibrary *assetsLibrary;
+
 @end
 
 @implementation ViewController
 
 @synthesize captureManager = _captureManager;
+@synthesize assetsLibrary = _assetsLibrary;
 
+- (ALAssetsLibrary *) assetsLibrary
+{
+    if (!_assetsLibrary) {
+        _assetsLibrary = [[ALAssetsLibrary alloc] init];
+    }
+    return _assetsLibrary;
+}
+    
 - (void)handleTap:(UITapGestureRecognizer *)recognizer {
     [self.captureManager captureStillImage];
 }
@@ -61,11 +72,24 @@
 
 - (void)captureManager:(DCCaptureManager *)captureManager didCaptureStillImage:(NSData *)jpegImageData withMetadata:(NSDictionary *)EXIFdata
 {
-    NSLog(@"captureManager:didCaptureStillImage with EXIT data %@", EXIFdata);
+    NSLog(@"captureManager:didCaptureStillImage with EXIF data %@", EXIFdata);
     
     // write to camera roll
+#if 1
+    // Allocate only one ALAssetsLibrary
+    [self.assetsLibrary writeImageDataToSavedPhotosAlbum:jpegImageData metadata:EXIFdata completionBlock:nil];
+#else
+    // memory leaks if you allocate ALAssetsLibrary each time
+    // Apple's AVCam sample code also leaks memory
+    // the leak is intermittent
+    // if you take about 20 pictures, you should see a memory leak in Instruments
+    // Malloc 48 bytes by [ALAssetsLibrary init]
     ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+    
+    // memory does not leak if yo do not call writeImageDataToSavedPhotosAlbum:metadata:completionBlock:
+    EXIFdata = nil; // make sure this is not causing the problem
     [library writeImageDataToSavedPhotosAlbum:jpegImageData metadata:EXIFdata completionBlock:nil];
+#endif
 }
 
 @end
